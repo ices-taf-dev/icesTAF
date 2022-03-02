791f0f0da41586452b892181d8ee1d6a61f54ed8
Arni Magnusson <thisisarni@gmail.com>
2022-03-02 13:50:48 +1100

    Import and export all of TAF

---
+++ R/icesTAF-import.R
@@ -0,0 +1,169 @@
+#' @name icesTAF-import
+#'
+#' @title -
+#'
+#' @description
+#' All TAF functions are imported into icesTAF to allow indirect namespace calls
+#' such as \code{icesTAF::read.taf}, which can be beneficial. The direct
+#' \code{TAF::read.taf} call also works.
+#'
+#' This comes with the minor cost that two help pages exist. The actual user
+#' documentation of imported TAF functions are found in the TAF package.
+#'
+#' @seealso
+#' \code{\link{TAF-package}} provides help pages for functions defined in the
+#' \pkg{TAF} package.
+#'
+#' \code{\link{icesTAF-package}} gives an overview of the \pkg{icesTAF} package.
+#'
+#' @import TAF
+#'
+#' @aliases
+#' already.in.taf.library
+#' clean
+#' clean.data
+#' clean.library
+#' clean.software
+#' convert.spaces
+#' cp
+#' deps
+#' detach.packages
+#' div
+#' dos2unix
+#' download
+#' download.github
+#' draft.data
+#' draft.software
+#' ds.file
+#' ds.package
+#' extract.subdir
+#' file.encoding
+#' flr2taf
+#' get.remote.sha
+#' is.r.package
+#' latin1.to.utf8
+#' lim
+#' line.endings
+#' long2taf
+#' long2xtab
+#' make
+#' makeAll
+#' makeTAF
+#' mkdir
+#' msg
+#' os
+#' os.linux
+#' os.macos
+#' os.unix
+#' os.windows
+#' parse.repo
+#' period
+#' plus
+#' process.bibfile
+#' process.entry
+#' read.bib
+#' read.taf
+#' rmdir
+#' rnd
+#' sam2taf
+#' sourceAll
+#' sourceDir
+#' sourceTAF
+#' stamp.description
+#' taf.boot.path
+#' taf.bootstrap
+#' taf.data.path
+#' taf.install
+#' taf.libPaths
+#' taf.library
+#' taf.png
+#' taf.session
+#' taf.skeleton
+#' taf.sources
+#' taf.unzip
+#' taf2long
+#' taf2xtab
+#' tafpng
+#' tt
+#' unix2dos
+#' utf8.to.latin1
+#' write.taf
+#' xtab2long
+#' xtab2taf
+#' zoom
+#' zoom.trellis
+#'
+#' @export already.in.taf.library
+#' @export clean
+#' @export clean.data
+#' @export clean.library
+#' @export clean.software
+#' @export convert.spaces
+#' @export cp
+#' @export deps
+#' @export detach.packages
+#' @export div
+#' @export dos2unix
+#' @export download
+#' @export download.github
+#' @export draft.data
+#' @export draft.software
+#' @export ds.file
+#' @export ds.package
+#' @export extract.subdir
+#' @export file.encoding
+#' @export flr2taf
+#' @export get.remote.sha
+#' @export is.r.package
+#' @export latin1.to.utf8
+#' @export lim
+#' @export line.endings
+#' @export long2taf
+#' @export long2xtab
+#' @export make
+#' @export makeAll
+#' @export makeTAF
+#' @export mkdir
+#' @export msg
+#' @export os
+#' @export os.linux
+#' @export os.macos
+#' @export os.unix
+#' @export os.windows
+#' @export parse.repo
+#' @export period
+#' @export plus
+#' @export process.bibfile
+#' @export process.entry
+#' @export read.bib
+#' @export read.taf
+#' @export rmdir
+#' @export rnd
+#' @export sam2taf
+#' @export sourceAll
+#' @export sourceDir
+#' @export sourceTAF
+#' @export stamp.description
+#' @export taf.boot.path
+#' @export taf.bootstrap
+#' @export taf.data.path
+#' @export taf.install
+#' @export taf.libPaths
+#' @export taf.library
+#' @export taf.png
+#' @export taf.session
+#' @export taf.skeleton
+#' @export taf.sources
+#' @export taf.unzip
+#' @export taf2long
+#' @export taf2xtab
+#' @export tafpng
+#' @export tt
+#' @export unix2dos
+#' @export utf8.to.latin1
+#' @export write.taf
+#' @export xtab2long
+#' @export xtab2taf
+#' @export zoom
+
+NA
